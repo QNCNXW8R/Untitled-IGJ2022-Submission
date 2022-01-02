b978d2e2907a5d9ac2ebb1fbdf31298d0fe39cb7
Daniel Gray <danielrcgray@gmail.com>
2022-01-02 21:54:56 +0000

    Add two upgrades

--- src/App.svelte
+++ src/App.svelte
@@ -2,17 +2,18 @@
     import TimeUnit from './Classes/TimeUnit'
     import TimeUnitRelation from './Classes/TimeUnitRelation'
     import TimeUnitPipeline from './Classes/TimeUnitPipeline'
+    import Upgrade from './Classes/Upgrade'
 
     let speedMult = 1
 
     var seconds = new TimeUnit('Seconds')
-    var minutes = new TimeUnit('Minutes', 0, 0, function() {return seconds.total >= 30}) //, function() {return seconds.total >= 30}) // Require upgrade here in future
-    var hours = new TimeUnit('Hours', 0, 0, function() {return minutes.total >= 30})
-    var days = new TimeUnit('Days', 0, 0, function() {return hours.total >= 12})
-    var years = new TimeUnit('Years', 0, 0, function() {return days.total >= 90})
-    var epochs = new TimeUnit('Epochs', 0, 0, function() {return years.total >= 1000})
-    var eons = new TimeUnit('Eons', 0, 0, function() {return epochs.total >= 1000})
-    var heatDeaths = new TimeUnit('Heat Deaths', 0, 0, function() {return eons.total >= 1000})
+    var minutes = new TimeUnit('Minutes', 0, 0, function() {return seconds.total >= 30}, function() {return seconds.total >= 60}) // Require upgrade here in future
+    var hours = new TimeUnit('Hours', 0, 0, function() {return minutes.total >= 30}, function() {return minutes.total >= 60})
+    var days = new TimeUnit('Days', 0, 0, function() {return hours.total >= 12}, function() {return hours.total >= 24})
+    var years = new TimeUnit('Years', 0, 0, function() {return days.total >= 90}, function() {return days.total >= 365})
+    var epochs = new TimeUnit('Epochs', 0, 0, function() {return years.total >= 1000}, function() {return years.total >= 10000})
+    var eons = new TimeUnit('Eons', 0, 0, function() {return epochs.total >= 1000}, function() {return epochs.total >= 100000})
+    var heatDeaths = new TimeUnit('Heat Deaths', 0, 0, function() {return eons.total >= 1000}, function() {return eons.total >= 1000000})
 
     var secondsToMinutes = new TimeUnitRelation(seconds, minutes, 60, 60, 10)
     var minutesToHours = new TimeUnitRelation(minutes, hours, 60, 60, 15)
@@ -23,6 +24,11 @@
     var eonsToHeatDeaths = new TimeUnitRelation(eons, heatDeaths, 1000000, 1000000, 1000000)
 
     var pipeline = new TimeUnitPipeline([secondsToMinutes, minutesToHours, hoursToDays, daysToYears, yearsToEpochs, epochsToEons, eonsToHeatDeaths])
+
+    var upgrades = [
+        new Upgrade('Double Minutes', function() {return minutes.total >= 0}, minutes, 10, function(){minutes.multiplier.multiplyFactor(2)}),
+        new Upgrade('Triple Minutes', function() {return minutes.total >= 0}, minutes, 15, function(){minutes.multiplier.multiplyFactor(3)})
+    ]
 
     var points = 0
 
@@ -37,6 +43,16 @@
         console.log("Stopping...")
         clearInterval(intervalID)
         intervalID = -1
+    }
+
+    function attemptPurchase(upgrade) {
+        let success = upgrade.purchase()
+        if (success) {
+            upgrades = upgrades.filter((value) => value.name !== upgrade.name);
+            console.log('Successfully purchased upgrade "' + upgrade.name + '"');
+        } else {
+            'Purchase failed'
+        }
     }
 
     function onTick() {
@@ -103,3 +119,11 @@
 
 <p id=pointsAmount>Points: {Math.floor(points)}</p>
 <p>Points are speeding up time by a factor of: {(1 + Math.log(points+1)).toPrecision(5)}</p>
+
+<ul>
+    {#each upgrades as upgrade}
+        {#if upgrade.visible()}
+            <li><button on:click={function() {attemptPurchase(upgrade)}}>{upgrade.name}: {upgrade.cost} {upgrade.costUnit.name}</button></li>
+        {/if}
+    {/each}
+</ul>
--- src/Classes/Upgrade.js
+++ src/Classes/Upgrade.js
@@ -1,22 +1,22 @@
 import TimeUnit from "./TimeUnit";
 import Multiplier from "./Multiplier";
 
-export default class TimeUnit {
-    constructor(name='', visible=function(){return true}, costUnit=TimeUnit(), cost=0, upgradeTarget=Multiplier().addBase, upgradeValue=1) {
+export default class Upgrade {
+    constructor(name='', visible=function(){return true}, costUnit=new TimeUnit(), cost=0, upgradeTarget=function(){new Multiplier().multiplyFactor(1)}) {
         this.name = name;
         this.visible = visible;
         this.costUnit = costUnit;
         this.cost = cost;
         this.upgradeTarget = upgradeTarget;
-        this.upgradeValue = upgradeValue;
     }
 
     purchase() {
         if (this.visible() && this.costUnit.value >= this.cost) {
-            console.log('Purchased upgrade "' + this.name + '"');
             this.costUnit.value -= this.cost;
             this.visible = function(){return false};
-            this.upgradeTarget(this.upgradeValue)
+            this.upgradeTarget()
+            return true
         }
+        return false
     }
 }
